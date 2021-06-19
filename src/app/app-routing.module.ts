6f8cd622e7b1a3883143b78c8cd7e823a2bf7141
sandra963 <sandra25vilca@gmail.com>
2021-06-19 05:16:05 -0500

    templete listo con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
 
-const routes: Routes = [];
+import{ Routes, RouterModule} from '@angular/router';
+import { ViajeComponent } from "./pages/viaje/viaje.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { PaqueteturisticoComponent } from "./pages/paqueteturistico/paqueteturistico.component";
+import { AlojamientoComponent } from "./pages/alojamiento/alojamiento.component";
+import { LoginComponent } from "./pages/login/login.component";
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+const app_routes: Routes=[
+  { path: '', component: ViajeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'alojamiento', component:AlojamientoComponent},
+ {path: '**', pathMatch:'full', redirectTo:''}
+
+];
+
+@NgModule({  
+   imports:[
+     RouterModule.forRoot( app_routes)
+  ],
+  exports:[
+    RouterModule
+  ],
+  }
+)
+export class AppRoutingModule{
+
+}
